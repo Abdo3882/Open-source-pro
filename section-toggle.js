aef978e062d31679940be6ed192bb9f1226c9210
Abdo-Hussien <114018757+Abdo3882@users.noreply.github.com>
2025-12-16 19:53:46 +0200

    Add files via upload

---
+++ section-toggle.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll(".section");
+
+sections.forEach((section) => {
+  const card = section.querySelector(".section__title");
+  card.addEventListener("click", () => {
+    sections.forEach((item) => {
+      if(item !== section) {
+        item.classList.remove("show-text");
+      }
+    });
+    section.classList.toggle("show-text");
+  });
+
+});
+
